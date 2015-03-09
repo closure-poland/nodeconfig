af05fde06baf3750511ac669fe40a7733b78bb21
Akkenoth <akkenoth@gmail.com>
2015-03-09 14:34:47 +0100

    JSONFileConfigProvider added to index

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.ConfigProvider = require('./ConfigProvider');
 module.exports.EnvironmentConfigProvider = require('./EnvironmentConfigProvider');
+module.exports.JSONFileConfigProvider = require('./JSONFileConfigProvider');
 module.exports.utils = require('./utils');
