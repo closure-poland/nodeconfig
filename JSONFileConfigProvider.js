3857ddbe6736976f8b696633e91dc9d668130a9d
Robert Kawecki <robert+github@rkaw.pl>
2015-03-27 00:58:40 +0100

    Simplified the error-checking code of JSONFileConfigProvider and made it also catch and propagate JSON parse errors as events properly.

--- JSONFileConfigProvider.js
+++ JSONFileConfigProvider.js
@@ -4,14 +4,17 @@ var fs = require('fs');
 function customize(baseProviderConstructor) {
 	function JSONFileConfigProvider(filename) {
 		var provider = new baseProviderConstructor();
-		fs.readFile(filename, { encoding: 'utf8' }, function populateConfig(err, data) {
-			if(err) {
-				console.error(err);
-				provider.getEmitter().emit('error', err);
-				return;
+		fs.readFile(filename, { encoding: 'utf8' }, function populateConfig(readError, data) {
+			try{
+				if (readError) {
+					throw readError;
+				}
+				var config = JSON.parse(data);
+				provider.setConfig(config);
 			}
-			var config = JSON.parse(data);
-			provider.setConfig(config);
+			catch(error) {
+				provider.getEmitter().emit('error', err);
+			}
 		});
 		return provider.getEmitter();
 	}
